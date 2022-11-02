56fb1adae1379bf2685b3f3904f6380f492f7b21
ItsMrDav <davutt.simsek@gmail.com>
2022-11-02 23:42:53 +0100

    done

--- app.js
+++ app.js
@@ -15,14 +15,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('index');
 });
-app.get('/beers', (req, res) => {
-  res.render('beers');
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersApi => {
+      res.render('beers', { beersApi });
+    })
+    .catch(err => console.log(err));
 });
-app.get('/random-beers', (req, res) => {
-  res.render('random-beers');
+
+app.get('/randomBeer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      console.log('Heres a random beer', randomBeer);
+      res.render('randomBeer', { randomBeer });
+    })
+    .catch(err => console.log(err));
+});
+
+app.get('/beerId/:id', (req, res) => {
+  const { id } = req.params;
+  punkAPI
+    .getBeer(id)
+    .then(singleBeer => {
+      console.log('Heres a single beer', singleBeer);
+      res.render('singleBeer', { singleBeer });
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
